09769a026cd244357b8a150d27c595a2d6370c1f
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2022-01-11 01:58:55 +0100

    Multi-challenge game

--- wordle.c
+++ wordle.c
@@ -32,8 +32,15 @@ unsigned char dict_file;
 
 unsigned short dict_size;
 
-unsigned char freq[100];
-unsigned char hint[100];
+unsigned short freq[400];
+unsigned short hint[400];
+
+unsigned char insert_secret_words;
+
+unsigned short score;
+
+unsigned short total_score;
+
 
 unsigned short read_dict(unsigned char dict_file)
 {
@@ -85,7 +92,7 @@ unsigned short read_dict(unsigned char dict_file)
     return count;
 }
 
-void reset_freq(unsigned char *vect)
+void reset_freq(unsigned short *vect)
 {
     unsigned char i;
     
@@ -142,16 +149,196 @@ void instructions(void)
 
 }
 
-
-int main(int argc, char **argv)
+unsigned short compute_score(unsigned char word_found, unsigned char attempt_number, unsigned char exact_matches, clock_t elapsed_time)
 {
+    unsigned short base_score;
+    
+    unsigned short score = 0;
+    
+    unsigned short bonus = 0;
+    
+    unsigned short time_penalty = 0;
+    
+    if(!word_found)
+    {
+        base_score = exact_matches * 50;
+    }
+    else
+    {
+        base_score = 900;
+        
+        if (attempt_number<6)
+        {
+            bonus = 100;
+        }
+        
+        if(elapsed_time>300)
+        {
+            time_penalty = 300;
+        }
+        else if (elapsed_time>60)
+        {
+            time_penalty = elapsed_time - 60;
+        }
+    }
+    
+    // printf("exact_matches: %d\n", exact_matches);
+    // printf("base_score   : %5d\n", base_score);
+    // printf("bonus        : %5d\n", bonus);
+    // printf("time_penalty : %5d\n", time_penalty);
+    
+    return base_score + bonus - time_penalty;
+}
+
+
+void challenge(void)
+{  
     unsigned char attempt_number;
-    char yn[5];
     unsigned char i;
     unsigned char j;
     unsigned char char_found;
     unsigned char word_found;
-    unsigned char insert_secret_words;
+    unsigned char exact_matches;
+    
+    clock_t start_t;
+    clock_t elapsed_time;
+
+    if(!insert_secret_words)
+    {
+    
+        secret_index = rand()%dict_size;
+        strcpy(secret,dict[secret_index]);
+    }
+    else
+    {
+        do
+        {
+            printf("Insert secret word\n");
+            scanf("%5s", secret);
+        } while(!in_dict(secret));
+    }
+    
+    compute_freq();
+    
+    // for(i='a';i<'z';++i)
+    // {
+        // printf("%d", freq[i]);
+    // }
+
+
+    printf("\n\n--------------------------------\n");
+    
+    #if defined(DEBUG)
+        printf("secret: %s\n", secret);
+    #endif
+    
+
+    attempt_number = 1;
+    word_found = 0;
+    
+    start_t = clock() / (CLOCKS_PER_SEC);
+    while(attempt_number<=MAX_ATTEMPTS)
+    {
+        printf("\nTry no. %d\n", attempt_number);
+        scanf("%5s", attempt);
+        
+        if(!strcmp("x",attempt))
+        {
+            break;
+        }
+        
+        if(!in_dict(attempt))
+        {
+            printf("Not in dictionary\n");
+        }
+        else
+        {
+            
+            reset_freq(hint);
+            
+            ++attempt_number;
+            if(!strcmp(secret,attempt))
+            {
+                word_found = 1;
+                break;
+            }
+            else
+            {
+                // First compute frequencies of exact matches
+                exact_matches = 0;
+                for(i=0;i<5;++i)
+                {
+                    if(secret[i]==attempt[i])
+                    {
+                        ++hint[attempt[i]];
+                        ++exact_matches;
+                    }
+                }           
+                
+                // Compute matches and partial matches
+                for(i=0;i<5;++i)
+                {
+                    if(secret[i]==attempt[i])
+                    {
+                        printf("%c",secret[i]);
+                    }
+                    else
+                    {
+                        for(j=0;j<5;++j)
+                        {
+                            char_found = 0;
+                            if((secret[j]==attempt[i]) && 
+                               (hint[attempt[i]]<freq[attempt[i]]) // # hints of a letter in wrong position <= # occurrences of the letter in secret word
+                               ) 
+                            {
+                                char_found = 1;
+                                break;
+                            }
+                        }
+                        if(char_found)
+                        {    
+                            printf("*");
+                            ++hint[attempt[i]];
+                        }
+                        else
+                        {
+                            printf("-");
+                        }
+                    }
+                }
+                printf("\n");
+            }  
+        }
+    }
+
+    --attempt_number;
+    printf("\n\n");
+    
+    if(word_found)
+    {
+        printf("YOU WIN!\n");
+    }
+    else
+    {
+        printf("YOU FAILED!\n");
+        printf("Secret word: %s\n", secret);
+    }
+    
+    elapsed_time = clock() / (CLOCKS_PER_SEC) - start_t;
+    printf("Attempts: %d\n", attempt_number);
+    printf("Time: %d\n", elapsed_time);
+    score = compute_score(word_found, attempt_number, exact_matches, elapsed_time);
+
+    
+}
+
+
+int main(int argc, char **argv)
+{
+    unsigned char i;
+    unsigned short number_of_challenges;
+    
+    char yn[5];
 
     printf("\n\n\n-----------------------------------------------------------------------------\n");
     printf("                      WORDLE\n");
@@ -181,124 +368,28 @@ int main(int argc, char **argv)
         insert_secret_words = 1;
     }
 
+    printf("\nHow many challenges per game?");
+    scanf("%d", &number_of_challenges);
+    
     while(1){
         
-        if(!insert_secret_words)
+        total_score = 0;
+        for(i=1;i<=number_of_challenges;++i)
         {
-        
-            secret_index = rand()%dict_size;
-            strcpy(secret,dict[secret_index]);
-        }
-        else
-        {
-            do
-            {
-                printf("Insert secret word\n");
-                scanf("%5s", secret);
-            } while(!in_dict(secret));
-        }
-        
-        compute_freq();
-        
-        // for(i='a';i<'z';++i)
-        // {
-            // printf("%d", freq[i]);
-        // }
-        
-        printf("\n\n--------------------------------\n");
-        
-        #if defined(DEBUG)
-            printf("secret: %s\n", secret);
-        #endif
-        
+            printf("\n-----------------------------------------------------------------------------\n");
+            printf("Challenge no. %d\n", i);
+            printf(  "-----------------------------------------------------------------------------\n");
 
-        attempt_number = 1;
-        word_found = 0;
-        while(attempt_number<=MAX_ATTEMPTS)
-        {
-            printf("\nTry no. %d\n", attempt_number);
-            scanf("%5s", attempt);
-            
-            if(!strcmp("x",attempt))
+            challenge();
+            printf("SCORE: %5d", score);
+            if(score==1000)
             {
-                break;
+                printf(" MAX SCORE !");
             }
-            
-            if(!in_dict(attempt))
-            {
-                printf("Not in dictionary\n");
-            }
-            else
-            {
-                
-                reset_freq(hint);
-                
-                ++attempt_number;
-                if(!strcmp(secret,attempt))
-                {
-                    word_found = 1;
-                    break;
-                }
-                else
-                {
-                    // First compute frequencies of exact matches
-                    for(i=0;i<5;++i)
-                    {
-                        if(secret[i]==attempt[i])
-                        {
-                            ++hint[attempt[i]];
-                        }
-                    }           
-                    
-                    // Compute matches and partial matches
-                    for(i=0;i<5;++i)
-                    {
-                        if(secret[i]==attempt[i])
-                        {
-                            printf("%c",secret[i]);
-                        }
-                        else
-                        {
-                            for(j=0;j<5;++j)
-                            {
-                                char_found = 0;
-                                if((secret[j]==attempt[i]) && 
-                                   (hint[attempt[i]]<freq[attempt[i]]) // # hints of a letter in wrong position <= # occurrences of the letter in secret word
-                                   ) 
-                                {
-                                    char_found = 1;
-                                    break;
-                                }
-                            }
-                            if(char_found)
-                            {    
-                                printf("*");
-                                ++hint[attempt[i]];
-                            }
-                            else
-                            {
-                                printf("-");
-                            }
-                        }
-                    }
-                    printf("\n");
-                }  
-            }
+            printf("\n\n\n");
+            total_score+=score;
+            printf("TOTAL SCORE: %5d\n", total_score);
         }
-        
-        printf("\n\n");
-        
-        if(word_found)
-        {
-            printf("YOU WIN!\n");
-        }
-        else
-        {
-            printf("YOU FAILED!\n");
-            printf("Secret word: %s\n", secret);
-        }
-        
-        printf("\n\n");
         
         do
         {
@@ -313,16 +404,6 @@ int main(int argc, char **argv)
         {
             break;
         }
-        // printf("attempt: %s\n", attempt);
-        
-        // if(in_dict(attempt))
-        // {
-            // printf("In dictionary\n");
-        // }
-        // else
-        // {
-            // printf("Not in dictionary\n");
-        // }
     }
     
     return EXIT_SUCCESS;
