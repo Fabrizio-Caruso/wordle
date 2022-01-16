d819627c95c34fca795cec4d8cc7fc9ed20ff9ea
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2022-01-16 23:18:23 +0100

    Portuguese

--- wordle.c
+++ wordle.c
@@ -12,23 +12,29 @@
 
 #define DICTIONARY_DIR "dictionaries/"
 
-#define ENG_3_DICT_FILE DICTIONARY_DIR "3_letter_words_ENG.txt"
-#define ENG_4_DICT_FILE DICTIONARY_DIR "4_letter_words_ENG.txt"
-#define ENG_5_DICT_FILE DICTIONARY_DIR "5_letter_words_ENG.txt"
-#define ENG_6_DICT_FILE DICTIONARY_DIR "6_letter_words_ENG.txt"
-#define ENG_7_DICT_FILE DICTIONARY_DIR "7_letter_words_ENG.txt"
-#define ENG_8_DICT_FILE DICTIONARY_DIR "8_letter_words_ENG.txt"
+#define ENG_DIR "ENG/"
+#define FRA_DIR "FRA/" 
+#define ITA_DIR "ITA/"
+#define ROM_DIR "ROM/"
+#define ESP_DIR "ESP/"
+#define DEU_DIR "DEU/"
+#define POR_DIR "POR/"
 
+#define ENG_3_DICT_FILE DICTIONARY_DIR ENG_DIR "3_letter_words_ENG.txt"
+#define ENG_4_DICT_FILE DICTIONARY_DIR ENG_DIR "4_letter_words_ENG.txt"
+#define ENG_5_DICT_FILE DICTIONARY_DIR ENG_DIR "5_letter_words_ENG.txt"
+#define ENG_6_DICT_FILE DICTIONARY_DIR ENG_DIR "6_letter_words_ENG.txt"
+#define ENG_7_DICT_FILE DICTIONARY_DIR ENG_DIR "7_letter_words_ENG.txt"
+#define ENG_8_DICT_FILE DICTIONARY_DIR ENG_DIR "8_letter_words_ENG.txt"
 
 #define ENG_DICT_FILE ENG_5_DICT_FILE
 
-
-#define FRA_DICT_FILE DICTIONARY_DIR "5_letter_words_FRA.txt"
-#define ITA_DICT_FILE DICTIONARY_DIR "5_letter_words_ITA.txt"
-#define ROM_DICT_FILE DICTIONARY_DIR "5_letter_words_ROM.txt"
-#define ESP_DICT_FILE DICTIONARY_DIR "5_letter_words_ESP.txt"
-#define DEU_DICT_FILE DICTIONARY_DIR "5_letter_words_DEU.txt"
-
+#define FRA_DICT_FILE DICTIONARY_DIR "FRA/" "5_letter_words_FRA.txt"
+#define ITA_DICT_FILE DICTIONARY_DIR "ITA/" "5_letter_words_ITA.txt"
+#define ROM_DICT_FILE DICTIONARY_DIR "ROM/" "5_letter_words_ROM.txt"
+#define ESP_DICT_FILE DICTIONARY_DIR "ESP/" "5_letter_words_ESP.txt"
+#define DEU_DICT_FILE DICTIONARY_DIR "DEU/" "5_letter_words_DEU.txt"
+#define POR_DICT_FILE DICTIONARY_DIR "POR/" "5_letter_words_POR.txt"
 
 #define ENG 1
 #define FRA 2
@@ -36,6 +42,7 @@
 #define ROM 4
 #define ESP 5
 #define DEU 6
+#define POR 7
 
 #define NOT_TRIED 0
 #define TRIED_AND_NOT_FOUND 1
@@ -112,6 +119,10 @@ unsigned short read_dict(unsigned char dict_file)
         
         case DEU:
             fd = fopen(DEU_DICT_FILE, "r");
+        break;
+        
+        case POR:
+            fd = fopen(POR_DICT_FILE, "r");
         break;
         
         case ENG:
@@ -534,7 +545,7 @@ int main(int argc, char **argv)
         printf("        ANSI C version by Fabrizio Caruso\n");
 
         instructions();
-        printf("\nChoose language\n1 = English, 2 = French, 3 = Italian, 4 = Romanian, 5 = Spanish, 6 = German :");
+        printf("\nChoose language\n1 = English, 2 = French, 3 = Italian, 4 = Romanian, 5 = Spanish, 6 = German, 7 = Portuguese :");
         
         scanf("%d", &dict_file);
         
