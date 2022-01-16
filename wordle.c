3e561f7f02f6106b9d3c04588f4fc8b0694bd760
Fabrizio Caruso <fabrizio_caruso@hotmail.com>
2022-01-16 14:51:12 +0100

    German support

--- wordle.c
+++ wordle.c
@@ -10,21 +10,24 @@
 
 #define MAX_DICT_SIZE 50000
 
-#define ENG_3_DICT_FILE "3_letter_words_ENG.txt"
-#define ENG_4_DICT_FILE "4_letter_words_ENG.txt"
-#define ENG_5_DICT_FILE "5_letter_words_ENG.txt"
-#define ENG_6_DICT_FILE "6_letter_words_ENG.txt"
-#define ENG_7_DICT_FILE "7_letter_words_ENG.txt"
-#define ENG_8_DICT_FILE "8_letter_words_ENG.txt"
+#define DICTIONARY_DIR "dictionaries/"
+
+#define ENG_3_DICT_FILE DICTIONARY_DIR "3_letter_words_ENG.txt"
+#define ENG_4_DICT_FILE DICTIONARY_DIR "4_letter_words_ENG.txt"
+#define ENG_5_DICT_FILE DICTIONARY_DIR "5_letter_words_ENG.txt"
+#define ENG_6_DICT_FILE DICTIONARY_DIR "6_letter_words_ENG.txt"
+#define ENG_7_DICT_FILE DICTIONARY_DIR "7_letter_words_ENG.txt"
+#define ENG_8_DICT_FILE DICTIONARY_DIR "8_letter_words_ENG.txt"
 
 
 #define ENG_DICT_FILE ENG_5_DICT_FILE
 
 
-#define FRA_DICT_FILE "5_letter_words_FRA.txt"
-#define ITA_DICT_FILE "5_letter_words_ITA.txt"
-#define ROM_DICT_FILE "5_letter_words_ROM.txt"
-#define ESP_DICT_FILE "5_letter_words_ESP.txt"
+#define FRA_DICT_FILE DICTIONARY_DIR "5_letter_words_FRA.txt"
+#define ITA_DICT_FILE DICTIONARY_DIR "5_letter_words_ITA.txt"
+#define ROM_DICT_FILE DICTIONARY_DIR "5_letter_words_ROM.txt"
+#define ESP_DICT_FILE DICTIONARY_DIR "5_letter_words_ESP.txt"
+#define DEU_DICT_FILE DICTIONARY_DIR "5_letter_words_DEU.txt"
 
 
 #define ENG 1
@@ -32,6 +35,7 @@
 #define ITA 3
 #define ROM 4
 #define ESP 5
+#define DEU 6
 
 #define NOT_TRIED 0
 #define TRIED_AND_NOT_FOUND 1
@@ -104,6 +108,10 @@ unsigned short read_dict(unsigned char dict_file)
         
         case ESP:
             fd = fopen(ESP_DICT_FILE, "r");
+        break;
+        
+        case DEU:
+            fd = fopen(DEU_DICT_FILE, "r");
         break;
         
         case ENG:
@@ -526,7 +534,7 @@ int main(int argc, char **argv)
         printf("        ANSI C version by Fabrizio Caruso\n");
 
         instructions();
-        printf("\nChoose language\n1 = English, 2 = French, 3 = Italian, 4 = Romanian, 5 = Spanish :");
+        printf("\nChoose language\n1 = English, 2 = French, 3 = Italian, 4 = Romanian, 5 = Spanish, 6 = German :");
         
         scanf("%d", &dict_file);
         
